8f58e0e73cc8a202fb0d4fc047dc92749e9c3bfd
arturocespedes <arturo.cespedes.f@mail.pucv.cl>
2024-05-23 12:52:14 -0400

    TraslaWeb Prueba 43

--- assets/js/importer.js
+++ assets/js/importer.js
@@ -1,3 +1,43 @@
+
+
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("nav-active");
+  this.classList.toggle("active");
+});
+
+/**
+ * toggle the navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    header.classList.toggle("nav-active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    
+  } else {
+    header.classList.remove("active");
+    ;
+  }
+});
+
+
 /**
  * dark patterns - taller de diseño de interacción e[ad] 2024
  * Este código está diseñado para cargar y mostrar casos de estudio sobre patrones oscuros en diseño de interacción desde una API.
